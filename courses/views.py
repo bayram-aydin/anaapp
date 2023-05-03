c61869c7965d50c2115780a62d8ff83f4c5c8f78
bayram-aydin <bayramaydin@msn.com>
2023-05-03 11:54:24 +0300

    lokal statik yapıldı

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
@@ -6,19 +7,65 @@ data = {
     "programlama":"programlaam işine ait kurlar",
     "web":"web işine ait kurlar",
     "mobil":"mobil işine ait kurlar işine ait kurlar",
+    "mobil-uygulama":"mobil işine ait kurlar işine ait kurlar",
+}
+
+db = {
+    "courses": [
+        {
+          "title":"Javascript Kursu",
+            "description":"Javascript Kurs Açıklaması",
+            "imageUrl":"02.jpg",
+            "slug":"java", 
+            "date": datetime.now(),
+            "isActive": True,
+            "isUpdated": True,
+        },
+        {
+          "title":"Python Kursu",
+            "description":"Python Kurs Açıklaması",
+            "imageUrl":"03.jpg",
+            "slug":"python", 
+            "date": date(2022,8,10),
+             "isActive": False,
+             "isUpdated": True,
+        },
+        {
+          "title":"Web Kursu",
+            "description":"Web Kurs Açıklaması",
+            "imageUrl":"04.jpg",
+            "slug":"web", 
+            "date": date(2022,7,10),
+             "isActive": True,
+             "isUpdated": False,
+        },
+    ],
+    "categories":[
+        {"id":1, "name":"programlama", "slug":"proglramlama"},
+        {"id":2, "name":"web geliştirme", "slug":"web-gelistirme"},
+        {"id":3, "name":"mobil uygulamalar", "slug":"mobil-uygulamalar"},
+        ]
 }
 
 def index (request):
-    return render(request, "courses/index.html")
-def kurslar (request):
-    return HttpResponse("Kurslar Sayfa") 
+    kurslar = [course for course in db ["courses"] if course ["isActive"]==True]
+    kategoriler = db["categories"]
+
+    return render(request, "courses/index.html",{
+        "categories": kategoriler,
+        "courses" : kurslar
+    })
+
 def details (request, kurs_adi):
     return HttpResponse(f"{kurs_adi} Detay Sayfası") 
 
 def getCoursesByCategory (request, category_name):
     try:
         category_text = data[category_name]
-        return HttpResponse(category_text) 
+        return render(request, "courses/kurslar.html", {
+            "category": category_name,
+            "category_text": category_text
+        })
     except:
         return HttpResponseNotFound("doğru düzgün bir kategori seç adam ol")
 def getCoursesByCategoryId(request, category_id):
